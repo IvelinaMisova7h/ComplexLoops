d9b5c9a752e73704897a6c223f230f00f5df54e5
ivelinamisova <ivelina_misova@abv.bg>
2022-06-07 12:01:49 +0300

    init

---
+++ factorial_calculation.py
@@ -0,0 +1,6 @@
+n = int(input())
+result = 1
+
+for num in range(n, 0, -1):
+    result *= num
+print(result)
---
+++ series_of_numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+result = 1
+
+for num in range(0, n):
+    print(result)
+    result = result * 2 + 1
+    if result > n:
+        break
